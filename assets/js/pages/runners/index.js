0bdd617cded22642bbadc2205611ac79a9158458
jm1021 <jmort1021@gmail.com>
2026-08-31 06:32:49 -0700

    more integration for gamify support

--- assets/js/pages/runners/index.js
+++ assets/js/pages/runners/index.js
@@ -7,11 +7,16 @@ export { ButtonFeedback } from './core/ButtonFeedback.js';
 export { CodeExecutor } from './executors/CodeExecutor.js';
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 export { PseudocodeExecutor } from './executors/PseudocodeExecutor.js';
 =======
 >>>>>>> 3750c40 (_project and support for site.baseurl)
 =======
 export { PseudocodeExecutor } from './executors/PseudocodeExecutor.js';
 >>>>>>> 1b4d221 (update for code runner features and editing.)
+=======
+export { PseudocodeExecutor } from './executors/Pseudosystem_Executors/PseudocodeExecutor.js';
+export { RobotExecutor } from './executors/Pseudosystem_Executors/RobotExecutor.js';
+>>>>>>> bf9a9ea (more integration for gamify support)
 export { UiExecutor } from './executors/UiExecutor.js';
 export { GameExecutor } from './executors/GameExecutor.js';
